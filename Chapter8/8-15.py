b9cbd77cfee1d2d041e90750d7a17102ca8eeeba
jochigt87 <contrerasjochimin@gmail.com>
2018-07-14 20:54:29 -0400

    PythonCrashCourse

--- Chapter8/8-15.py
+++ Chapter8/8-15.py
@@ -6,13 +6,3 @@ import printing_models
 # Printing Models: Put  the functions for the example printing_models.py in a
 # separate file called printing_functions.py. Write an import statement at the
 # top of printing_models.py, and modify the file to use the imported functions.
-
-
-# testing
-# testing
-# testing
-# testing
-# testing
-# testing
-# testing
-# testing
